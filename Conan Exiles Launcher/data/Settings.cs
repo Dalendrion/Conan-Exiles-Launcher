deb27b8dd9945984ed609ac650191284efc0fd04
Arjen Vermaas <analysk@gmail.com>
2026-05-19 10:16:41 +0200

    Tweak settings.

--- Conan Exiles Launcher/data/Settings.cs
+++ Conan Exiles Launcher/data/Settings.cs
@@ -6,9 +6,25 @@ namespace Conan_Exiles_Launcher.data
 {
     internal class Settings
     {
-        private string _steamPath = "D:\\SteamLibrary";
-        private string _gamePath = "\\steamapps\\common\\Conan Exiles\\ConanSandbox";
-        private string _modPath = "\\Mods\\modlist.txt";
-        private string _workshopContent = "\\steamapps\\workshop\\content\\440900";
+        private string _steamPath = "C:\\Users\\avermaas\\Desktop\\SteamLibrary\\";
+        private string _gamePath = "steamapps\\common\\Conan Exiles\\ConanSandbox\\";
+        private string _modPath = "Mods\\modlist.txt";
+        private string _workshopContent = "steamapps\\workshop\\content\\440900\\";
+
+        public String ModlistPath
+        {
+            get
+            {
+                return _steamPath + _gamePath + _modPath;
+            }
+        }
+
+        public string WorkshopPath
+        {
+            get
+            {
+                return _steamPath + _workshopContent;
+            }
+        }
     }
 }
